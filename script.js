f61980c6bd665fd25f1b22c0500646d0f15cb629
Shahid Amin <schboostie@gmail.com>
2023-04-22 14:54:13 +0100

    function added to script.js - makePageForEpisodes
    show the elements of the object on the page

--- script.js
+++ script.js
@@ -10,3 +10,34 @@ function makePageForEpisodes(episodeList) {
 }
 
 window.onload = setup;
+
+function makePageForEpisodes(episodeList) {
+  const rootElem = document.getElementById("root");
+
+  const headerElem = document.createElement("header");
+  headerElem.innerHTML = `<h1>All Episodes (${episodeList.length} episodes)</h1>`;
+  rootElem.appendChild(headerElem);
+
+  const episodesContainer = document.createElement("div");
+  episodesContainer.classList.add("episodes-container");
+
+  episodeList.forEach((episode) => {
+    const { id, name, season, number, image, summary } = episode;
+    const code = `S${season.toString().padStart(2, "0")}E${number
+      .toString()
+      .padStart(2, "0")}`;
+    const episodeElem = document.createElement("div");
+    episodeElem.classList.add("episode");
+    episodeElem.innerHTML = `
+      <img src="${image.medium}" alt="${name}" />
+      <div class="episode-info">
+        <h2>${name} - ${code}</h2>
+        ${summary ? summary : "<p>No summary available</p>"}
+        <p><a href="http://www.tvmaze.com/episodes/${id}" target="_blank">See on TVMaze.com</a></p>
+      </div>
+    `;
+    episodesContainer.appendChild(episodeElem);
+  });
+
+  rootElem.appendChild(episodesContainer);
+}
