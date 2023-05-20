f6c0e99d19b7f5958d8e08a04099fdb4d79630f4
Shahid Amin <schboostie@gmail.com>
2023-05-20 14:17:23 +0100

    practise100 files added

---
+++ practise100.js
@@ -0,0 +1,58 @@
+// Assuming you have a function getAllEpisodes() that returns an array of episode objects
+
+// Function to pad a number with leading zeros to two digits
+function padNumber(num) {
+  return num.toString().padStart(2, "0");
+}
+
+// Function to generate the episode code in the format S02E07
+function generateEpisodeCode(season, episode) {
+  const paddedSeason = padNumber(season);
+  const paddedEpisode = padNumber(episode);
+  return `S${paddedSeason}E${paddedEpisode}`;
+}
+
+// Function to create an episode card element
+function createEpisodeCard(episode) {
+  const episodeCard = document.createElement("div");
+  episodeCard.className = "episode-card";
+
+  const episodeName = document.createElement("h2");
+  episodeName.textContent = episode.name;
+  episodeCard.appendChild(episodeName);
+
+  const episodeCode = generateEpisodeCode(episode.season, episode.number);
+  const episodeInfo = document.createElement("p");
+  episodeInfo.textContent = `Episode Code: ${episodeCode}`;
+  episodeCard.appendChild(episodeInfo);
+
+  const episodeImage = document.createElement("img");
+  episodeImage.src = episode.image.medium;
+  episodeImage.alt = "Episode Image";
+  episodeCard.appendChild(episodeImage);
+
+  const episodeSummary = document.createElement("p");
+  episodeSummary.textContent = episode.summary;
+  episodeCard.appendChild(episodeSummary);
+
+  const episodeLink = document.createElement("a");
+  episodeLink.href = episode.url;
+  episodeLink.textContent = "View on TVMaze.com";
+  episodeCard.appendChild(episodeLink);
+
+  return episodeCard;
+}
+
+// Function to display all episodes on the page
+function displayEpisodes() {
+  const rootElement = document.getElementById("root");
+  const episodes = getAllEpisodes();
+
+  episodes.forEach((episode) => {
+    const episodeCard = createEpisodeCard(episode);
+    rootElement.appendChild(episodeCard);
+  });
+}
+
+// Call the displayEpisodes() function to populate the page with episodes
+displayEpisodes();
