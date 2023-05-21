82a5f3d6e37b5910021dac1f506cf76bc0597148
Shahid Amin <schboostie@gmail.com>
2023-05-21 16:40:25 +0100

    using fetch to retrieve data from api

--- practise350/300.js
+++ practise350/300.js
@@ -1,143 +1,85 @@
-let episodes = [];
-let filteredEpisodes = [];
-
-// Function to fetch the episodes for a specific show
+// Fetch episodes from the TVMaze API
 async function fetchEpisodes(showId) {
   try {
     const response = await fetch(
       `https://api.tvmaze.com/shows/${showId}/episodes`
     );
-    const data = await response.json();
-    episodes = data;
-    filteredEpisodes = data;
-    displayEpisodes();
-    populateEpisodeSelect();
+    const episodes = await response.json();
+    const processedEpisodes = episodes.map((episode) => ({
+      id: episode.id,
+      name: episode.name,
+      season: episode.season,
+      number: episode.number,
+      image: episode.image,
+      summary: episode.summary,
+      url: episode.url,
+    }));
+    displayEpisodes(processedEpisodes);
   } catch (error) {
     console.log("Error fetching episodes:", error);
   }
 }
 
-// Function to display the episodes
-function displayEpisodes() {
-  const rootElement = document.getElementById("root");
-  rootElement.innerHTML = "";
+// Function to pad a number with leading zeros to two digits
+function padNumber(num) {
+  return num.toString().padStart(2, "0");
+}
 
-  filteredEpisodes.forEach((episode) => {
+// Function to generate the episode code in the format S02E07
+function generateEpisodeCode(season, episode) {
+  const paddedSeason = padNumber(season);
+  const paddedEpisode = padNumber(episode);
+  return `S${paddedSeason}E${paddedEpisode}`;
+}
+
+// Function to create an episode card element
+function createEpisodeCard(episode) {
+  const episodeCard = document.createElement("div");
+  episodeCard.className = "episode-card";
+
+  const episodeName = document.createElement("h2");
+  episodeName.textContent = episode.name;
+  episodeCard.appendChild(episodeName);
+
+  const episodeCode = generateEpisodeCode(episode.season, episode.number);
+  const episodeInfo = document.createElement("p");
+  episodeInfo.textContent = `Episode Code: ${episodeCode}`;
+  episodeCard.appendChild(episodeInfo);
+
+  const episodeImage = document.createElement("img");
+  episodeImage.src = episode.image.medium;
+  episodeImage.alt = "Episode Image";
+  episodeCard.appendChild(episodeImage);
+
+  const episodeSummary = document.createElement("p");
+  episodeSummary.textContent = episode.summary;
+  episodeCard.appendChild(episodeSummary);
+
+  const episodeLink = document.createElement("a");
+  episodeLink.href = episode.url;
+  episodeLink.textContent = "View on TVMaze.com";
+  episodeCard.appendChild(episodeLink);
+
+  return episodeCard;
+}
+
+// Function to display all episodes on the page
+function displayEpisodes(episodes) {
+  const rootElement = document.getElementById("root");
+  rootElement.innerHTML = ""; // Clear previous episodes
+
+  episodes.forEach((episode) => {
     const episodeCard = createEpisodeCard(episode);
     rootElement.appendChild(episodeCard);
   });
 }
 
-// Function to create an episode card
-function createEpisodeCard(episode) {
-  const card = document.createElement("div");
-  card.className = "episode-card";
+// Function to initialize the page
+function initializePage() {
+  const showId = 82; // Replace with the actual show ID
 
-  const title = document.createElement("h2");
-  title.textContent = episode.name;
-  card.appendChild(title);
-
-  const seasonEpisode = document.createElement("p");
-  seasonEpisode.textContent = `S${episode.season
-    .toString()
-    .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}`;
-  card.appendChild(seasonEpisode);
-
-  const image = document.createElement("img");
-  image.src = episode.image.medium;
-  image.alt = "Episode Image";
-  card.appendChild(image);
-
-  const summary = document.createElement("p");
-  summary.textContent = episode.summary;
-  card.appendChild(summary);
-
-  const link = document.createElement("a");
-  link.href = episode.url;
-  link.textContent = "View on TVMaze.com";
-  card.appendChild(link);
-
-  return card;
+  fetchEpisodes(showId);
 }
 
-// Function to populate the episode select dropdown
-function populateEpisodeSelect() {
-  const selectElement = document.getElementById("episode-select");
-  selectElement.innerHTML = "";
-
-  const allOption = document.createElement("option");
-  allOption.value = "";
-  allOption.textContent = "All Episodes";
-  selectElement.appendChild(allOption);
-
-  filteredEpisodes.forEach((episode) => {
-    const option = document.createElement("option");
-    option.value = episode.id;
-    option.textContent = `S${episode.season
-      .toString()
-      .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")} - ${
-      episode.name
-    }`;
-    selectElement.appendChild(option);
-  });
-}
-
-// Function to handle episode select change event
-function handleEpisodeSelect(event) {
-  const selectedEpisodeId = event.target.value;
-  if (selectedEpisodeId) {
-    filteredEpisodes = episodes.filter(
-      (episode) => episode.id === parseInt(selectedEpisodeId)
-    );
-  } else {
-    filteredEpisodes = episodes;
-  }
-  displayEpisodes();
-}
-
-// Function to handle search input
-function handleSearchInput(event) {
-  const searchTerm = event.target.value.trim().toLowerCase();
-  filteredEpisodes = episodes.filter(
-    (episode) =>
-      episode.name.toLowerCase().includes(searchTerm) ||
-      episode.summary.toLowerCase().includes(searchTerm)
-  );
-  displayEpisodes();
-}
-
-// Function to handle show select change event
-function handleShowSelect(event) {
-  const selectedShowId = event.target.value;
-  if (selectedShowId) {
-    fetchEpisodes(selectedShowId);
-  }
-}
-
-/// Event listeners
-document
-  .getElementById("search-input")
-  .addEventListener("input", handleSearchInput);
-document
-  .getElementById("episode-select")
-  .addEventListener("change", handleEpisodeSelect);
-document
-  .getElementById("show-select")
-  .addEventListener("change", handleShowSelect);
-
-// Fetch shows and populate the show select dropdown
-getAllShows().then((shows) => {
-  const showSelect = document.getElementById("show-select");
-
-  // Sort shows in alphabetical order (case-insensitive)
-  shows.sort((a, b) =>
-    a.name.toLowerCase().localeCompare(b.name.toLowerCase())
-  );
-
-  shows.forEach((show) => {
-    const option = document.createElement("option");
-    option.value = show.id;
-    option.textContent = show.name;
-    showSelect.appendChild(option);
-  });
-});
+// Call the initializePage() function to populate the page with episodes
+initializePage();
